c5e73cb23c492beac5091937597ccab1043fd40a
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-09 06:55:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function wakeDog(dogName, dogBreed){
+  let result = `Wake ${dogName} the ${dogBreed}`
+  console.log(result)
+  return result
+}
+function leashDog(dogName, dogBreed){
+  let result = `Leash ${dogName} the ${dogBreed}`
+  console.log(result)
+  return result
+}
+function walkToPark(dogName, dogBreed){
+  let result = `Walk to the park with ${dogName} the ${dogBreed}`
+  console.log(result)
+  return result
+}
+function throwFrisbee(dogName, dogBreed){
+  let result = `Throw the frisbee for ${dogName} the ${dogBreed}`
+  console.log(result)
+  return result
+}
+function walkHome(dogName, dogBreed){
+  let result = `Wake ${dogName} the ${dogBreed}`
+  console.log(result)
+  return result
+}
+function unleashDog(dogName, dogBreed){
+  let result = `Wake ${dogName} the ${dogBreed}`
+  console.log(result)
+  return result
+}
